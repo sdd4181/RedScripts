0939dad33128e14acbe708c1cdbb417acfa9fd06
sdd4181 <sdd4181@rit.edu>
2022-02-26 12:59:37 -0500

    added a rdp setup

---
+++ RDP setup.ps1
@@ -0,0 +1,5 @@
+﻿$password = ConvertTo-SecureString "asdfasdf" -AsPlainText -Force
+Write-Output $password
+New-LocalUser "AdminScoring" -Password $password -FullName "Scoring Account" -Description "Scoring Acc."
+Add-LocalGroupMember -Group "Administrators" -Member "AdminScoring"
+Add-LocalGroupMember -Group "Remote Desktop Users" -Member "AdminScoring"
