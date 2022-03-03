cc24462ee92684ddf7ef04378e5b76a9f3716ac4
sdd4181 <sdd4181@rit.edu>
2022-03-03 18:14:28 -0500

    active directory 1 way trust should work, will test soon

---
+++ AD1wayTrust.ps1
@@ -0,0 +1,39 @@
+﻿# Change following parameters
+
+$strRemoteForest = "ISTS.com"
+
+$strRemoteAdmin = Read-Host "enter account name"
+
+$strRemoteAdminPassword = Read-Host "enter account password"
+
+$remoteContext = New-Object -TypeName "System.DirectoryServices.ActiveDirectory.DirectoryContext" -ArgumentList @( "Forest", $strRemoteForest, $strRemoteAdmin, $strRemoteAdminPassword)
+
+try {
+
+        $remoteForest = [System.DirectoryServices.ActiveDirectory.Forest]::getForest($remoteContext)
+
+    }
+
+catch {
+
+        Write-Warning "GetRemoteForest: Failed:`n`tError: $($($_.Exception).Message)"
+
+    }
+
+
+
+$localforest=[System.DirectoryServices.ActiveDirectory.Forest]::getCurrentForest()
+
+
+try {
+
+        $localForest.CreateTrustRelationship($remoteForest,"Inbound")
+
+
+    }
+
+catch {
+
+        Write-Warning "CreateTrustRelationship: Failed for domain $($remoteForest)`n`tError: $($($_.Exception).Message)"
+
+    }
